1940a6249b02ca82bf8efbcc1ac1548522fec4aa
Darshan Patel <29084562+darshanpatel1992@users.noreply.github.com>
2018-03-07 06:20:21 -0500

    Add files via upload

---
+++ Association_Rules/Apriori/Apriori.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  5 02:12:50 2018
+
+@author: darshan patel
+"""
+#Apriori Algorithm
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing dataset
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header=None)
+transactions= []
+for i in range(0,7501):
+    transactions.append([str(dataset.values[i,j]) for j in range(0,20)])
+
+#Training Apriori on the dataset
+from apyori import apriori
+rules= apriori(transactions,min_support=0.003,min_confidence=0.2,min_lift=3,min_length=2)    
+
+#Visualizing the result
+result=list(rules)
